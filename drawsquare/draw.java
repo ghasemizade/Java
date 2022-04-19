1c27aab07635e5eda3dc2cef1e7f1bf1dac82b8e
Hosein <mrqasemizade@gmail.com>
2022-04-19 18:21:31 +0430

    draw square

---
+++ drawsquare/draw.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+class MyCanvas extends JComponent {
+
+  public void paint(Graphics g) {
+    g.drawRect (10, 10, 200, 200);  
+  }
+}
+
+public class draw {
+  public static void main(String[] a) {
+    JFrame window = new JFrame();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setBounds(30, 30, 300, 300);
+    window.getContentPane().add(new MyCanvas());
+    window.setVisible(true);
+  }
+}
