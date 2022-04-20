0ccb9a77b156f43f846a835b7d619ad982e3d0fe
Hosein <mrqasemizade@gmail.com>
2022-04-20 13:36:43 +0430

    Draw triangle Z

---
+++ Draw triangle Z/Draw.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class  Draw{
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter the number :");
+
+    int input = myObj.nextInt();
+    //int sum = 1;
+    for (int counter = 1; counter < input; counter++)
+    {
+      System.out.println("* ");
+    }
+    System.out.println("\n");
+
+    for (int counter1 = 1; counter1 < counter; counter1++) 
+    {
+      System.out.print("");
+    }
+    System.out.print("\n");
+  }
+}
